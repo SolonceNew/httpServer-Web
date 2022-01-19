a39d252dd3555d641936caad3c9e702580fc5085
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2022-01-19 22:24:32 +0300

    added final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Server server = new Server();
+        final var server = new Server();
         server.start();
         }
 
